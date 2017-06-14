424851d493618e5c389ae718189b99fd7cf3cb37
jack0088 at codea-scm <jack0088@codea-scm.aws.mapote.com>
2017-06-14 21:13:23 +0000

    we had header oauth parameters already working - now query parameters work too; also began writing pin-based token request functionality

--- tabs/Main.lua
+++ tabs/Main.lua
@@ -7,8 +7,8 @@
 local app_consumer_key = "dVJWptRAumKSM5r9DY2zoL9No"
 local app_consumer_secret = "27hxObUkFS5Yqu0zO4uHx1Gjh4zh2LJbKEDJQ457kx3r9Fpzz7"
 -- this access token can be used to make api requests on your own accounts behalf
-local app_access_token = "345353224-iixJTqNLWlZFMsQif6kisDuPKIno9so2fmZYTAqN"
-local app_access_token_secret = "gUelacqps4NVe0CPH5fUrudZq5yc8aMLO4j6mzEcNJLMM"
+local account_access_token = "345353224-iixJTqNLWlZFMsQif6kisDuPKIno9so2fmZYTAqN"
+local account_access_token_secret = "gUelacqps4NVe0CPH5fUrudZq5yc8aMLO4j6mzEcNJLMM"
 
 
 local DROPBOX = os.getenv("HOME").."/Documents/Dropbox.assets"
@@ -30,7 +30,7 @@ end
 
 
 local function generate_random_string(length)
-    local charset = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890"
+    local charset = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789"
     local rndnum  = math.random(1, #charset)
     local rndchar = charset:sub(rndnum, rndnum)
     if length > 0 then return generate_random_string(length - 1)..rndchar end
@@ -38,9 +38,10 @@ local function generate_random_string(length)
 end
 
 
-local function build_signature(method, url, parameters)
+local function build_signature(method, url, parameters, consumer_secret, access_token_secret)
     local list = {}
     local order = {}
+    local key = rfc_3986_encode(consumer_secret).."&"..rfc_3986_encode(access_token_secret)
     local signature = ""
     
     for key, value in pairs(parameters) do
@@ -56,17 +57,13 @@ local function build_signature(method, url, parameters)
         signature = signature.."&"..key.."="..value
     end
     
-    return method:upper().."&"..rfc_3986_encode(url).."&"..rfc_3986_encode(signature:sub(2))
-end
-
-
-local function encode_signature(signature, consumer_secret, access_token_secret)
-    local sign_key = rfc_3986_encode(consumer_secret).."&"..rfc_3986_encode(access_token_secret)
+    signature = method:upper().."&"..rfc_3986_encode(url).."&"..rfc_3986_encode(signature:sub(2))
+    
     return base_64_encode(
         hmac()
         .setBlockSize(64)
         .setDigest(sha1)
-        .setKey(array_encode(sign_key))
+        .setKey(array_encode(key))
         .init()
         .update(stream_encode(signature))
         .finish()
@@ -88,6 +85,43 @@ local function build_authorization_header(parameters)
     return header:sub(1, -2)
 end
 
+local function build_query_url(url, parameters)
+    local prefix = "oauth_"
+    local request = url.."?"
+    
+    for key, value in pairs(parameters) do
+        if not key:find(prefix) then
+            request = request..key.."="..tostring(value).."&"
+        end
+    end
+    
+    return request:sub(1, -2)
+end
+
+
+local function request_account_access_token()
+    local url = {
+        {"POST", "https://api.twitter.com/oauth/request_token"},
+        {"GET", "https://api.twitter.com/oauth/authorize"},
+        {"POST", "https://api.twitter.com/oauth/access_token"}
+    }
+    
+    
+    local request = {"POST", "https://api.twitter.com/oauth/request_token", {
+        oauth_callback = "oob", -- pin based authentication
+        oauth_version = "1.0",
+        oauth_nonce = generate_random_string(32),
+        oauth_timestamp = os.time() + 1,
+        oauth_consumer_key = app_consumer_key,
+        oauth_signature_method = "HMAC-SHA1",
+        oauth_signature
+    }}
+    
+    local authorize = {"GET", "https://api.twitter.com/oauth/authorize"}
+    local access = {"POST", "https://api.twitter.com/oauth/access_token"}
+
+end
+
 
 local function report_request_success(response, status, headers)
     print(status, type(headers))
@@ -102,37 +136,28 @@ local function report_request_failure(error)
 end
 
 
-local function make_api_request(method, url, parameters)
+local function request_api(method, url, parameters)
     parameters.oauth_version = "1.0"
     parameters.oauth_nonce = generate_random_string(32)
     parameters.oauth_timestamp = os.time() + 1
     parameters.oauth_consumer_key = app_consumer_key
-    parameters.oauth_token = app_access_token
+    parameters.oauth_token = account_access_token
     parameters.oauth_signature_method = "HMAC-SHA1"
-    parameters.oauth_signature = encode_signature(build_signature(method, url, parameters), app_consumer_secret, app_access_token_secret)
+    parameters.oauth_signature = build_signature(method, url, parameters, app_consumer_secret, account_access_token_secret)
     
-    http.request(url, report_request_success, report_request_failure, {
+    http.request(build_query_url(url, parameters), report_request_success, report_request_failure, {
         method = method:upper(),
-        headers = {
-            --["Accept"] = "*/*",
-            --["Connection"] = "close http header",
-            --["Host"] = "api.twitter.com",
-            --["User-Agent"] = "OAuth gem v0.4.4",
-            --["Content-Type"] = "application/x-www-form-urlencoded",
-            ["Authorization"] = build_authorization_header(parameters),
-            --["Content-Length"] = "76",
-            --["status"] = "hello world, this is a test"
-        }
+        headers = {Authorization = build_authorization_header(parameters)}
     })
 end
 
 
 function setup()
-    --make_api_request("GET", "https://api.twitter.com/1.1/statuses/home_timeline.json", {})
-    make_api_request("GET", "https://api.twitter.com/1.1/statuses/user_timeline.json", {})
-    --make_api_request("GET", "https://api.twitter.com/1.1/followers/list.json", {})
-    --make_api_request("GET", "https://api.twitter.com/1.1/statuses/retweets_of_me.json", {})
-    --make_api_request("GET", "https://api.twitter.com/1.1/favorites/list.json", {})
+    --request_api("GET", "https://api.twitter.com/1.1/statuses/home_timeline.json", {})
+    request_api("GET", "https://api.twitter.com/1.1/statuses/user_timeline.json", {count = 1})
+    --request_api("GET", "https://api.twitter.com/1.1/followers/list.json", {})
+    --request_api("GET", "https://api.twitter.com/1.1/statuses/retweets_of_me.json", {})
+    --request_api("GET", "https://api.twitter.com/1.1/favorites/list.json", {})
 end
 
 
