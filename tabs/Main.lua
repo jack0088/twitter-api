9497104ff4feef5175e174b8e9c224e669e6d2d6
jack0088 at codea-scm <jack0088@codea-scm.aws.mapote.com>
2017-06-15 12:40:18 +0000

    updated example

--- tabs/Main.lua
+++ tabs/Main.lua
@@ -1,10 +1,14 @@
 function setup()
     -- Display one tweet from user's home timeline
-    twitter.request("GET", "https://api.twitter.com/1.1/statuses/home_timeline.json", {count = 1}, function(response)
-        local content = json.decode(response)
-        tweet = content[1].user.name.." wrote\n"..content[1].text
-        print(tweet)
-    end)
+    local function get_tweet()
+        twitter.request("GET", "https://api.twitter.com/1.1/statuses/home_timeline.json", {count = 1}, function(response)
+            local content = json.decode(response)
+            tweet = content[1].user.name.." wrote\n"..content[1].text
+            print(tweet)
+        end)
+    end
+    
+    twitter.check(get_tweet)
 end
 
 
